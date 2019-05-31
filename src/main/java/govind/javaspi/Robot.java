16a979098986427736081ffdce3a36ba25db51d0
高文文 <gaowenwencn@163.com>
2019-05-31 15:47:15 +0800

    dubbo spi机制实现

--- src/main/java/govind/javaspi/Robot.java
+++ src/main/java/govind/javaspi/Robot.java
@@ -1,5 +1,8 @@
 package govind.javaspi;
 
+import govind.spi.SPI;
+
+@SPI("Robot")
 public interface Robot {
 	void sayHello();
 }
--- src/test/java/govind/javaspi/TestRobot.java
+++ src/test/java/govind/javaspi/TestRobot.java
@@ -1,14 +1,26 @@
 package govind.javaspi;
 
+import govind.spi.ExtensionLoader;
 import org.junit.Test;
 
 import java.util.ServiceLoader;
 
 public class TestRobot {
 	@Test
-	public void sayHello() {
+	public void testJavaSPI() {
 		ServiceLoader<Robot>  loader = ServiceLoader.load(Robot.class);
 		System.out.println("Java SPI");
 		loader.forEach(Robot::sayHello);
 	}
+
+	@Test
+	public void testDubboSPI() {
+		ExtensionLoader<Robot> loader = ExtensionLoader.getExtensionLoader(Robot.class);
+		Robot optimusPrime = loader.getExtension("optimusPrime");
+  		optimusPrime.sayHello();
+
+		Robot bumblebee = loader.getExtension("bumblebee");
+		bumblebee.sayHello();
+	}
+
 }
